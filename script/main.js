f8c77253acac64771fa25783efb423ca5c4e348a
mayangsar15 <mayang.sari01605@gmail.com>
2025-08-24 18:38:08 +0800

    update responsive layout

--- script/main.js
+++ script/main.js
@@ -1,20 +1,27 @@
+// Fading links
 document.querySelectorAll('a[href$=".html"]').forEach(link => {
   link.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.body.style.opacity = 0;
-    setTimeout(() => {
-      window.location.href = this.href;
-    }, 300);
+    // Cek kalau link bukan tombol toggle
+    if (!this.classList.contains('menu-toggle')) {
+      e.preventDefault();
+      document.body.style.opacity = 0;
+      setTimeout(() => {
+        window.location.href = this.href;
+      }, 300);
+    }
   });
 });
 
+// Page load fade-in
 window.addEventListener('load', () => {
   document.body.style.opacity = 1;
 });
 
+// Toggle nav
 const toggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('nav ul');
 
-toggle.addEventListener('click', () => {
+toggle.addEventListener('click', (e) => {
+  e.stopPropagation(); // pastikan tidak tertangkap event bubbling
   menu.classList.toggle('active');
 });
